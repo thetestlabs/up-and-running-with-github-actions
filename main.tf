4dd800249146014b2b92b4a1beddea020fa3eb70
Chris McQuaid <chris@thestestlabs.com>
2021-10-24 15:10:23 +0100

    Added tfsec workflow

--- main.tf
+++ main.tf
@@ -1,5 +1,8 @@
-resource "null_resource" "example" {
-  triggers = {
-    value = "A example resource that does nothing!"
-  }
+resource "aws_security_group_rule" "my-insecure-rule" {
+  type              = "ingress"
+  from_port         = 0
+  to_port           = 0
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = "sg-123456"
 }
