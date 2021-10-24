b6a16dc2a3d268f1ebf6e24b20e9d590594136e8
Chris McQuaid <chris@thestestlabs.com>
2021-10-24 10:57:53 +0100

    Formatted Terraform

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
-    resource "null_resource" "example" {
-      triggers = {
-        value = "A example resource that does nothing!"
-      }
-    }
+resource "null_resource" "example" {
+  triggers = {
+    value = "A example resource that does nothing!"
+  }
+}
